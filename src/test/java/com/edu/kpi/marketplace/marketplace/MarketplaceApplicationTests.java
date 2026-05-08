c7871731ba4f7c7d1469d76300f828c05fab78e9
Volodymyr Dyachenko <vov.dyachenko@gmail.com>
2026-05-08 22:44:12 +0300

    Project is added

---
+++ src/test/java/com/edu/kpi/marketplace/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.kpi.marketplace.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
